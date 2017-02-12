a2e78922cb5a64a3a50598f7ae278e4fe6f8ceb5
lisevich <lisevich@slice.com>
2017-02-12 12:36:20 +0200

    commit

---
+++ HillelHomeWorks/src/lesson5/Main.java
@@ -0,0 +1,12 @@
+package lesson5;
+
+
+/**
+ * Created by mandarin80 on 12.02.17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Reversion reversion = new Reversion();
+        System.out.println(reversion.revertAll("123456"));
+    }
+}
---
+++ src/lesson5/Main.java
@@ -0,0 +1,12 @@
+package lesson5;
+
+/**
+ * Created by mandarin80 on 09.02.17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Test ivanov = new Test();
+        ivanov.setFirstName("Ivanov");
+        System.out.println(ivanov.getFirstName());
+    }
+}
