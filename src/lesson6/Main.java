6d2c84b25bcf1f4f407cd1f9292d311a8d106956
lisevich <lisevich@slice.com>
2017-02-19 16:59:25 +0200

    commit

--- src/lesson6/Main.java
+++ src/lesson6/Main.java
@@ -5,26 +5,17 @@ package lesson6;
  */
 public class Main {
     public static void main(String[] args) {
-        Container container = new Container(1);
-        container.add(2);
-        container.add(3);
-        for (int i = 0; i < container.getSize(); i++) {
-            System.out.println(container.get(i));
-        }
-        System.out.println(container.contains(2));
-        int[] tail = {5,6,8,7};
+        // creation of new Student;
+        Students ivanov = new Students(1, "Vasiliy", "Ivanov");
+        System.out.println(ivanov.getId() + ", " + ivanov.getName());
+        ivanov.addMark(5);
+        ivanov.setLastName("Petrov-Vodkin");
+        System.out.println(ivanov.getId() + ", " + ivanov.getName());
+        ivanov.addMark(4);
+        ivanov.addMark(3);
+        ivanov.addMark(2);
 
-        container.addAll(tail);
-        for (int i = 0; i < container.getSize(); i++) {
-            System.out.println(container.get(i));
-        }
-
-        //container.clean();
-        container.sort(container, 0, container.getSize() - 1);
-        System.out.println("Sorted array");
-        for (int i = 0; i < container.getSize(); i++) {
-            System.out.print(container.get(i) + ", ");
-        }
+        System.out.println(ivanov.getName() + " has " + ivanov.numberOfMarks() + " marks");
 
     }
 }
--- src/lesson6/Students.java
+++ src/lesson6/Students.java
@@ -4,5 +4,51 @@ package lesson6;
  * Created by mandarin80 on 13.02.17.
  */
 public class Students {
+    private final int id;
+    private final String firstName;
+    private String lastName;
+    private Container marks = new Container(-5000);
+    public Students (int id, String firstName, String lastName){
+        this.id = id;
+        this.firstName = firstName;
+    }
+
+    public void addMark(int mark) {
+        if (marks.getSize() == 1 && marks.get(0) == -5000) {
+            this.marks.set(0, mark);
+        } else {
+            this.marks.add(mark);
+        }
+    }
+
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public String getName(){
+        return firstName + " " + lastName;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public int getMark(int index){
+        return marks.get(index);
+    }
+
+    public boolean containsMark(int elementForCheck){
+        return marks.contains(elementForCheck);
+    }
+
+    public void setMark(int index, int value){
+        marks.set(index, value);
+    }
+
+    public int numberOfMarks(){
+        return marks.getSize();
+    }
+
 
 }
